791309ad0727e5373194814e7101f2f95bf9edbe
letdebbycook <debbygusdiahkbar205@gmail.com>
2026-01-06 15:50:07 +0700

    minor fix

--- iris.py
+++ iris.py
@@ -93,4 +93,4 @@ def index():
 # RUN APP
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
